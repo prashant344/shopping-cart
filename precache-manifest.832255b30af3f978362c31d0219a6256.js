a405ddc94174ca3a18e932ac1621da427a3892fb
prashant ranjan <prashantranjan@prashants-MacBook-Air.local>
2020-04-14 10:27:59 +0530

    Updates

---
+++ precache-manifest.832255b30af3f978362c31d0219a6256.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7cd6f25fc3b689bba093798ac8701115",
+    "url": "/shopping-cart/index.html"
+  },
+  {
+    "revision": "038ede3afc44cd404940",
+    "url": "/shopping-cart/static/css/main.31acc96b.chunk.css"
+  },
+  {
+    "revision": "e46c416ea42b963c0854",
+    "url": "/shopping-cart/static/js/2.c2c6a618.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/shopping-cart/static/js/2.c2c6a618.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "038ede3afc44cd404940",
+    "url": "/shopping-cart/static/js/main.3589bd2d.chunk.js"
+  },
+  {
+    "revision": "c214a4b73c0ce2e3c03b",
+    "url": "/shopping-cart/static/js/runtime-main.e527f4e6.js"
+  }
+]);
